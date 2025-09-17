bc9286f064874a5a93b4bf466c3d6064d68f0d3f
EasonMo <moyicheng1990@gmail.com>
2025-09-18 00:18:09 +0800

    feat: add pane index

--- scripts/fzf.sh
+++ scripts/fzf.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 GREEN="\033[32m"
+YELLOW="\033[33m"
 RESET="\033[0m"
 
 session_count=$(tmux list-sessions | wc -l)
 
 # 只有一个 session，不显示 session_name
 if [ "$session_count" -eq 1 ]; then
-  candidates=$(tmux list-panes -a -F "#{session_name}:#{window_index}.#{pane_index} #{b:pane_current_path} : ${GREEN}#{pane_current_command}${RESET}")
+  candidates=$(tmux list-panes -a -F "#{session_name}:#{window_index}.#{pane_index} #{b:pane_current_path} : ${YELLOW}#{window_index}.#{pane_index}${RESET} : ${GREEN}#{pane_current_command}${RESET}")
 
 else
-  candidates=$(tmux list-panes -a -F "#{session_name}:#{window_index}.#{pane_index} #{session_name} : #{b:pane_current_path} : ${GREEN}#{pane_current_command}${RESET}")
+  candidates=$(tmux list-panes -a -F "#{session_name}:#{window_index}.#{pane_index} #{session_name} : #{b:pane_current_path} : ${YELLOW}#{window_index}.#{pane_index}${RESET} : ${GREEN}#{pane_current_command}${RESET}")
 fi
 
 selected=$(
